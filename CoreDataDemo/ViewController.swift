efd672f7b03eb28946f4a5edecaed60a908b992a
Sargon Zi <iamsargonzi@gmail.com>
2018-11-15 16:33:59 +0630

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Sargon Zi on 11/15/18.
+//  Copyright © 2018 PADC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
